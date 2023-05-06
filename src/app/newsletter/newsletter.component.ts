e699e0930b44318eaf9743016fb772d199c23261
anton.chernianu <a.chernianu@autodoc.eu>
2023-05-06 22:08:38 +0300

    fix

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-newsletter',
   templateUrl: './newsletter.component.html',
-  styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent {
 
